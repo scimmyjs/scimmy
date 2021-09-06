9efa924e0c2237596e53881b17f694df59d6c29b
Sam Lee-Lindsay <sleelin@gmail.com>
2021-09-06 22:01:38 +1000

    Add: coercion of values to conform with defined attributes in SCIM schema and attributes classes of provisioning integration

--- types/attribute.js
+++ types/attribute.js
@@ -60,12 +60,13 @@ export class Attribute {
      * @param {Boolean} [config.caseExact=false] - whether the attribute's contents is case sensitive
      * @param {Boolean|String} [config.mutable=true] - whether the attribute's contents is modifiable
      * @param {Boolean|String} [config.returned=true] - whether the attribute is returned in a response
-     * @param {Boolean} [config.required=false] - whether or not the attribute is required for the type instance to be valid
-     * @param {Boolean} [config.required=false] - whether or not the attribute is required for the type instance to be valid
-     * @param {Boolean} [config.required=false] - whether or not the attribute is required for the type instance to be valid
+     * @param {Boolean|String[]} [config.referenceTypes=false] - list of referenced types if attribute type is reference
+     * @param {String|Boolean} [config.uniqueness="none"] - the attribute's uniqueness characteristic
+     * @param {String} [config.direction="both"] - whether the attribute should be present for inbound, outbound, or bidirectional requests
      * @param {Attribute[]} [subAttributes] - if the attribute is complex, the sub-attributes of the attribute
      */
     constructor(type, name, config = {}, subAttributes = []) {
+        // TODO: validate attribute type, mutability, returned, and uniqueness values
         this.type = type;
         this.name = name;
         this.config = {
@@ -112,6 +113,84 @@ export class Attribute {
             returned: (typeof this.config.returned === "string" ? this.config.returned
                 : (this.config.returned ? "default" : "never")),
             ...(this.type !== "boolean" && this.config.uniqueness !== false ? {uniqueness: this.config.uniqueness} : {})
+        }
+    }
+    
+    /**
+     * Coerce a given value by making sure it conforms to attribute's characteristics
+     * @param {*|*[]} source - value to coerce and confirm conformity with attribute's characteristics
+     * @param {String} [direction] - whether to check for inbound, outbound, or bidirectional attributes
+     * @param {Boolean} [isComplexMultiValue=false] - indicates whether a coercion is for a single complex value in a collection of complex values
+     * @returns {String|String[]|Number|Boolean|Object|Object[]} the coerced value, conforming to attribute's characteristics
+     */
+    coerce(source, direction = "both", isComplexMultiValue = false) {
+        // Make sure the direction matches the attribute direction
+        if (["both", direction].includes(this.config.direction)) {
+            let {required, multiValued, canonicalValues} = this.config;
+            
+            // If the attribute is required, make sure it has a value
+            if (required && source === undefined)
+                throw new TypeError(`Required attribute ${this.name} is missing`);
+            // If the attribute is multi-valued, make sure its value is a collection
+            if (!isComplexMultiValue && multiValued && source !== undefined && !Array.isArray(source))
+                throw new TypeError(`Attribute ${this.name} expected to be a collection`);
+            // If the attribute has canonical values, make sure it matches
+            if (Array.isArray(canonicalValues) && (!(multiValued ? (source ?? []).every(v => canonicalValues.includes(v)) : canonicalValues.includes(source))))
+                throw new TypeError(`Attribute ${this.name} contains non-canonical value`);
+            
+            // If the source has a value, parse it
+            if (source !== undefined) switch (this.type) {
+                case "string":
+                    // Cast supplied values into strings
+                    return (multiValued ? source.map(v => String(v)) : String(source));
+                
+                case "dateTime":
+                    // Check if value is a valid date
+                    let validate = (v) => (v instanceof Date && v.toString() !== "Invalid Date");
+                    
+                    // Throw error if all values aren't valid dates
+                    if (!(multiValued ? source.every(validate) : validate(source)))
+                        throw new TypeError(`Attribute ${name} expected value to be a valid date`);
+                    
+                    // Convert date values to ISO strings
+                    return (multiValued ? source.map(v => new Date(v).toISOString()) : new Date(source).toISOString());
+                
+                case "boolean":
+                    // Cast supplied values into booleans
+                    return (multiValued ? source.map(v => !!v) : !!source);
+                
+                case "complex":
+                    // Prepare for a complex attribute's values
+                    let target = (!isComplexMultiValue ? [] : {});
+                    
+                    // Evaluate complex attribute's sub-attributes
+                    if (isComplexMultiValue) {
+                        // Go through each sub-attribute for coercion
+                        for (let subAttribute of this.subAttributes) {
+                            let {name, config: {required}} = subAttribute;
+                            
+                            // If the value is defined or required, coerce it
+                            if (name in source || required) {
+                                let value = subAttribute.coerce(source[name], direction);
+                                
+                                // If the value is not empty, apply it to the target
+                                if (value !== undefined) target[name] = value;
+                            }
+                        }
+                    } else {
+                        // Go through each value and coerce their sub-attributes
+                        for (let value of (multiValued ? source : [source])) {
+                            target.push(this.coerce(value, direction, true));
+                        }
+                    }
+                    
+                    // Return the collection, or the coerced complex value
+                    return (multiValued || isComplexMultiValue ? target : target.pop());
+                    
+                default:
+                    // TODO: decimal, integer, and reference handlers
+                    return source;
+            }
         }
     }
 }
--- types/schema.js
+++ types/schema.js
@@ -8,8 +8,8 @@ export class Schema {
      * Constructs an instance of a full SCIM schema definition
      * @param {String} name - friendly name of the SCIM schema
      * @param {String} id - URN namespace of the SCIM schema
-     * @param {String} description - a human-readable description of the schema
-     * @param {Attribute[]} attributes - attributes that make up the schema
+     * @param {String} [description=""] - a human-readable description of the schema
+     * @param {Attribute[]} [attributes=[]] - attributes that make up the schema
      */
     constructor(name = "", id = "", description = "", attributes = []) {
         // Store the schema name, ID, and description
@@ -21,8 +21,8 @@ export class Schema {
         this.attributes = [
             new Attribute("string", "id", {required: true, direction: "out", mutable: false, caseExact: true, uniqueness: "global"}),
             new Attribute("string", "externalId", {direction: "in", caseExact: true}),
-            new Attribute("complex", "meta", {required: true, direction: "out", mutable: false}, [
-                new Attribute("string", "resourceType", {required: true, direction: "out", mutable: false, caseExact: true}),
+            new Attribute("complex", "meta", {required: true, mutable: false}, [
+                new Attribute("string", "resourceType", {required: true, mutable: false, caseExact: true}),
                 new Attribute("dateTime", "created", {required: true, direction: "out", mutable: false}),
                 new Attribute("dateTime", "lastModified", {required: true, direction: "out", mutable: false}),
                 new Attribute("string", "location", {direction: "out", mutable: false}),
@@ -31,5 +31,29 @@ export class Schema {
             // Only include valid Attribute instances
             ...attributes.filter(attr => attr instanceof Attribute)
         ];
+    }
+    
+    /**
+     * Coerce a given value by making sure it conforms to all schema attributes' characteristics
+     * @param {Object} data - value to coerce and confirm conformity of properties to schema attributes' characteristics
+     * @param {String} [direction="both"] - whether to check for inbound, outbound, or bidirectional attributes
+     * @returns {Object} the coerced value, conforming to all schema attributes' characteristics
+     */
+    coerce(data, direction = "both") {
+        let target = {},
+            // Add schema's name as resource type to meta attribute
+            source = {...data, meta: {...(data?.meta ?? {}), resourceType: this.name}};
+        
+        // Go through all attributes and coerce them
+        for (let attribute of this.attributes) {
+            let {name} = attribute,
+                // Evaluate the coerced value
+                value = attribute.coerce(source[name], direction);
+            
+            // If it's defined, add it to the target
+            if (value !== undefined) target[name] = value;
+        }
+        
+        return target;
     }
 }
