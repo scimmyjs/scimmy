dae37d66c030e31112a46cb31297961b1008b120
Sam Lee-Lindsay <sleelin@gmail.com>
2021-09-08 20:54:38 +1000

    Chore: add jsdoc to SCIMError class in provisioning integration

--- types/error.js
+++ types/error.js
@@ -1,4 +1,14 @@
+/**
+ * SCIM Error
+ * @extends {Error}
+ */
 export class SCIMError extends Error {
+    /**
+     * Instantiate a new error with SCIM error details
+     * @param {Number} status - HTTP status code to be sent with the error
+     * @param {String} scimType - the SCIM detail error keyword as per [RFC7644§3.12]{@link https://datatracker.ietf.org/doc/html/rfc7644#section-3.12}
+     * @param {String} message - a human-readable description of what caused the error to occur
+     */
     constructor(status, scimType, message) {
         super(message);
         
