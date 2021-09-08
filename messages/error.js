a097592f2603495f39e9377e7b563e256599da07
Sam Lee-Lindsay <sleelin@gmail.com>
2021-09-08 21:51:10 +1000

    Fix: only add scimType property to SCIM error message if it has been defined

--- messages/error.js
+++ messages/error.js
@@ -2,7 +2,7 @@ export class Error {
     constructor(status, scimType, detail) {
         this.schemas = ["urn:ietf:params:scim:api:messages:2.0:Error"];
         this.status = status;
-        this.scimType = scimType;
         this.detail = detail;
+        if (!!scimType) this.scimType = scimType;
     }
 }
