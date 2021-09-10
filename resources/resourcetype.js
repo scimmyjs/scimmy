7aeb8fbd0adeb5f618a24f9303060a694328b5e1
Sam Lee-Lindsay <sleelin@gmail.com>
2021-09-10 20:53:47 +1000

    Fix: actually add ResourceType SCIM resource implementation

---
+++ resources/resourcetype.js
@@ -0,0 +1,52 @@
+import {Resource, Error as SCIMError} from "../types.js";
+import {ListResponse} from "../messages.js";
+import {ResourceType as ResourceTypeSchema} from "../schemas.js";
+import Resources from "../resources.js";
+
+/**
+ * SCIM ResourceType Resource
+ * @extends {Resource}
+ */
+export class ResourceType extends Resource {
+    /** @implements {Resource~endpoint} */
+    static get endpoint() {
+        return "/ResourceTypes";
+    }
+    
+    /** @implements {Resource~#basepath} */
+    static #basepath;
+    /** @implements {Resource~basepath} */
+    static basepath(path) {
+        if (path === undefined) return ResourceType.#basepath;
+        else if (ResourceType.#basepath === undefined)
+            ResourceType.#basepath = (path.endsWith(ResourceType.endpoint) ? path : `${path}${ResourceType.endpoint}`);
+        
+        return ResourceType;
+    }
+    
+    /**
+     * Instantiate a new SCIM ResourceType resource and parse any supplied parameters
+     * @implements {Resource#constructor}
+     */
+    constructor(params, ...rest) {
+        // Bail out if a resource is requested by filter
+        if (!!(typeof params === "string" ? rest[0] ?? {} : params ?? {}).filter)
+            throw new SCIMError(403, null, "ResourceType does not support retrieval by filter");
+        
+        super(params, ...rest);
+    }
+    
+    /** @implements {Resource#read} */
+    async read() {
+        if (!this.id) {
+            return new ListResponse(Object.entries(Resources.registered())
+                .map(([,R]) => new ResourceTypeSchema(R.describe(), ResourceType.basepath())));
+        } else {
+            try {
+                return new ResourceTypeSchema(Resources.registered()[this.id].describe(), ResourceType.basepath());
+            } catch {
+                throw new SCIMError(404, null, `ResourceType ${this.id} not found`);
+            }
+        }
+    }
+}
