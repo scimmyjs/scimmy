e5c3f42af6e1cf0275ecb43ae4e4d0bdc0230174
Sam Lee-Lindsay <sleelin@gmail.com>
2021-09-10 16:13:48 +1000

    Add: SCIM ResourceType schema implementation

--- schemas.js
+++ schemas.js
@@ -2,6 +2,7 @@ import {User} from "./schemas/user.js";
 import {Group} from "./schemas/group.js";
 import {EnterpriseUser} from "./schemas/enterpriseuser.js";
 export {User, Group, EnterpriseUser};
+export {ResourceType} from "./schemas/resourcetype.js";
 
 export default class Schemas {
     static User = User;
---
+++ schemas/resourcetype.js
@@ -0,0 +1,36 @@
+import {Schema, SchemaDefinition, Attribute} from "../types.js";
+
+/**
+ * SCIM ResourceType Schema
+ * @implements {Schema}
+ */
+export class ResourceType extends Schema {
+    /** @implements {Schema~schema} */
+    static get schema() {
+        return ResourceType.#schema;
+    }
+    
+    /** @implements {Schema~#schema} */
+    static #schema = new SchemaDefinition("ResourceType", "urn:ietf:params:scim:schemas:core:2.0:ResourceType", "Resource Type", [
+        new Attribute("string", "id", {direction: "out", mutable: false}),
+        new Attribute("string", "name", {direction: "out", required: true, mutable: false}),
+        new Attribute("string", "description", {direction: "out", mutable: false}),
+        new Attribute("reference", "endpoint", {direction: "out", required: true, mutable: false, referenceTypes: ["uri"]}),
+        new Attribute("reference", "schema", {direction: "out", required: true, mutable: false, caseExact: true, referenceTypes: ["uri"]}),
+        new Attribute("complex", "schemaExtensions", {direction: "out", required: true, mutable: false}, [
+            new Attribute("reference", "schema", {direction: "out", required: true, mutable: false, caseExact: true, referenceTypes: ["uri"]}),
+            new Attribute("boolean", "required", {direction: "out", required: true, mutable: false})
+        ])
+    ]);
+    
+    /**
+     * Instantiates a new resource type that conforms to the SCIM ResourceType schema definition
+     * @param {Object} resource - the source data to feed through the schema definition
+     * @param {String} [basepath] - the base path for resolution of a resource's location
+     */
+    constructor(resource, basepath) {
+        super();
+        this.schemas = [ResourceType.#schema.id];
+        Object.assign(this, ResourceType.#schema.coerce(resource, "out", basepath));
+    }
+}
