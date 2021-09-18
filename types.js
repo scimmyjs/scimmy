90e8df650a7e9ebfa210a865e778648953f91230
Sam Lee-Lindsay <sleelin@gmail.com>
2021-09-18 17:11:15 +1000

    Refactor [2/2]: move SCIM schema type class into separate schema.js module

--- types.js
+++ types.js
@@ -1,5 +1,6 @@
 export {Attribute} from "./types/attribute.js";
-export {Schema, SchemaDefinition} from "./types/definition.js";
+export {SchemaDefinition} from "./types/definition.js";
+export {Schema} from "./types/schema.js";
 export {Resource} from "./types/resource.js";
 export {Filter} from "./types/filter.js";
 export {SCIMError as Error} from "./types/error.js";
