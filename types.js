aba4ddc91fa89118797445699d1211faeb6f6fbc
Sam Lee-Lindsay <sleelin@gmail.com>
2021-09-18 17:05:24 +1000

    Refactor [1/2]: rename schema.js to definition.js in preparation of division of classes in SCIM schema type

--- types.js
+++ types.js
@@ -1,5 +1,5 @@
 export {Attribute} from "./types/attribute.js";
-export {Schema, SchemaDefinition} from "./types/schema.js";
+export {Schema, SchemaDefinition} from "./types/definition.js";
 export {Resource} from "./types/resource.js";
 export {Filter} from "./types/filter.js";
 export {SCIMError as Error} from "./types/error.js";
