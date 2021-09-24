87fd974d686cf7868cc4044e5940f213eb6f5a71
Sam Lee-Lindsay <sleelin@gmail.com>
2021-09-24 16:14:51 +1000

    Add: etag and authenticationSchemes type attributes missing from spec example in SCIM ServiceProviderConfig schema implementation

--- schemas/spconfig.js
+++ schemas/spconfig.js
@@ -33,7 +33,11 @@ export class ServiceProviderConfig extends Schema {
             new Attribute("complex", "sort", {required: true, mutable: false, description: "A complex type that specifies sort result options."}, [
                 new Attribute("boolean", "supported", {required: true, mutable: false, description: "A Boolean value specifying whether or not the operation is supported."})
             ]),
+            new Attribute("complex", "etag", {required: true, mutable: false, description: "A complex type that specifies ETag configuration options."}, [
+                new Attribute("boolean", "supported", {required: true, mutable: false, description: "A Boolean value specifying whether or not the operation is supported."})
+            ]),
             new Attribute("complex", "authenticationSchemes", {required: true, mutable: false, multiValued: true, description: "A complex type that specifies supported authentication scheme properties."}, [
+                new Attribute("string", "type", {required: true, mutable: false, canonicalValues: ["oauth", "oauth2", "oauthbearertoken", "httpbasic", "httpdigest"], description: "The authentication scheme."}),
                 new Attribute("string", "name", {required: true, mutable: false, description: "The common authentication scheme name, e.g., HTTP Basic."}),
                 new Attribute("string", "description", {required: true, mutable: false, description: "A description of the authentication scheme."}),
                 new Attribute("reference", "specUri", {mutable: false, referenceTypes: ["external"], description: "An HTTP-addressable URL pointing to the authentication scheme's specification."}),
