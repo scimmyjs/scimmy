6b82715d76ed17df016a0eb7c11ff8df5e1224e5
Sam Lee-Lindsay <sleelin@gmail.com>
2021-11-06 22:18:38 +1100

    Add(SCIMMY.Schemas): missing descriptions from attributes in all schemas

--- src/lib/schemas/group.js
+++ src/lib/schemas/group.js
@@ -14,11 +14,11 @@ export class Group extends Types.Schema {
     
     /** @private */
     static #definition = new Types.SchemaDefinition("Group", "urn:ietf:params:scim:schemas:core:2.0:Group", "Group", [
-        new Types.Attribute("string", "displayName", {required: true}),
-        new Types.Attribute("complex", "members", {multiValued: true, uniqueness: false}, [
-            new Types.Attribute("string", "value", {mutable: "immutable"}),
-            new Types.Attribute("reference", "$ref", {mutable: "immutable", referenceTypes: ["User", "Group"]}),
-            new Types.Attribute("string", "type", {mutable: "immutable", canonicalValues: ["User", "Group"]})
+        new Types.Attribute("string", "displayName", {required: true, description: "A human-readable name for the Group. REQUIRED."}),
+        new Types.Attribute("complex", "members", {multiValued: true, uniqueness: false, description: "A list of members of the Group."}, [
+            new Types.Attribute("string", "value", {mutable: "immutable", description: "Identifier of the member of this Group."}),
+            new Types.Attribute("reference", "$ref", {mutable: "immutable", referenceTypes: ["User", "Group"], description: "The URI corresponding to a SCIM resource that is a member of this Group."}),
+            new Types.Attribute("string", "type", {mutable: "immutable", canonicalValues: ["User", "Group"], description: "A label indicating the type of resource, e.g., 'User' or 'Group'."})
         ])
     ]);
     
--- src/lib/schemas/resourcetype.js
+++ src/lib/schemas/resourcetype.js
@@ -13,16 +13,26 @@ export class ResourceType extends Types.Schema {
     }
     
     /** @private */
-    static #definition = new Types.SchemaDefinition("ResourceType", "urn:ietf:params:scim:schemas:core:2.0:ResourceType", "Resource Type", [
-        new Types.Attribute("string", "name", {direction: "out", required: true, mutable: false}),
-        new Types.Attribute("string", "description", {direction: "out", mutable: false}),
-        new Types.Attribute("reference", "endpoint", {direction: "out", required: true, mutable: false, referenceTypes: ["uri"]}),
-        new Types.Attribute("reference", "schema", {direction: "out", required: true, mutable: false, caseExact: true, referenceTypes: ["uri"]}),
-        new Types.Attribute("complex", "schemaExtensions", {direction: "out", mutable: false, multiValued: true}, [
-            new Types.Attribute("reference", "schema", {direction: "out", required: true, mutable: false, caseExact: true, referenceTypes: ["uri"]}),
-            new Types.Attribute("boolean", "required", {direction: "out", required: true, mutable: false})
-        ])
-    ]);
+    static #definition = (() => {
+        let definition = new Types.SchemaDefinition("ResourceType", "urn:ietf:params:scim:schemas:core:2.0:ResourceType", "Specifies the schema that describes a SCIM resource type", [
+            new Types.Attribute("string", "name", {direction: "out", required: true, mutable: false, description: "The resource type name. When applicable, service providers MUST specify the name, e.g., 'User'."}),
+            new Types.Attribute("string", "description", {direction: "out", mutable: false, description: "The resource type's human-readable description. When applicable, service providers MUST specify the description."}),
+            new Types.Attribute("reference", "endpoint", {direction: "out", required: true, mutable: false, referenceTypes: ["uri"], description: "The resource type's HTTP-addressable endpoint relative to the Base URL, e.g., '/Users'."}),
+            new Types.Attribute("reference", "schema", {direction: "out", required: true, mutable: false, caseExact: true, referenceTypes: ["uri"], description: "The resource type's primary/base schema URI."}),
+            new Types.Attribute("complex", "schemaExtensions", {direction: "out", mutable: false, multiValued: true, uniqueness: false, description: "A list of URIs of the resource type's schema extensions."}, [
+                new Types.Attribute("reference", "schema", {direction: "out", required: true, mutable: false, caseExact: true, referenceTypes: ["uri"], description: "The URI of a schema extension."}),
+                new Types.Attribute("boolean", "required", {direction: "out", required: true, mutable: false, description: "A Boolean value that specifies whether or not the schema extension is required for the resource type. If true, a resource of this type MUST include this schema extension and also include any attributes declared as required in this schema extension. If false, a resource of this type MAY omit this schema extension."})
+            ])
+        ]);
+        
+        // Make the ID attribute visible!
+        Object.assign(definition.attribute("id").config, {
+            shadow: false, required: false, returned: true, caseExact: false, uniqueness: "none",
+            description: "The resource type's server unique id. May be the same as the 'name' attribute."
+        });
+        
+        return definition;
+    })();
     
     /**
      * Instantiates a new resource type that conforms to the SCIM ResourceType schema definition
