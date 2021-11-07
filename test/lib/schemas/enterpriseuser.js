b358917c1aa3cd8d3957ce67411d95a380b6498b
Sam Lee-Lindsay <sleelin@gmail.com>
2021-11-07 15:15:38 +1100

    Tests(SCIMMY.Schemas.EnterpriseUser): verify schema definition is accurate

--- test/lib/schemas/enterpriseuser.js
+++ test/lib/schemas/enterpriseuser.js
@@ -1,9 +1,16 @@
+import {promises as fs} from "fs";
+import path from "path";
+import url from "url";
 import assert from "assert";
 
-export let EnterpriseUserSuite = async (SCIMMY) => {
+export let EnterpriseUserSuite = (SCIMMY, SchemasHooks) => {
+    const basepath = path.relative(process.cwd(), path.dirname(url.fileURLToPath(import.meta.url)));
+    const fixtures = fs.readFile(path.join(basepath, "./enterpriseuser.json"), "utf8").then((f) => JSON.parse(f));
+    
     it("should include static class 'EnterpriseUser'", () => 
         assert.ok(!!SCIMMY.Schemas.EnterpriseUser, "Static class 'EnterpriseUser' not defined"));
     
     describe("SCIMMY.Schemas.EnterpriseUser", () => {
+        describe(".definition", SchemasHooks.definition(SCIMMY.Schemas.EnterpriseUser, fixtures));
     });
 }
