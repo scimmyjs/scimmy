d654bea9bea77c013d5c4491300985d683e3cde7
Sam Lee-Lindsay <sleelin@gmail.com>
2021-11-07 15:16:07 +1100

    Tests(SCIMMY.Schemas.User): verify schema definition is accurate

--- test/lib/schemas/user.js
+++ test/lib/schemas/user.js
@@ -1,9 +1,16 @@
+import {promises as fs} from "fs";
+import path from "path";
+import url from "url";
 import assert from "assert";
 
-export let UserSuite = async (SCIMMY) => {
+export let UserSuite = (SCIMMY, SchemasHooks) => {
+    const basepath = path.relative(process.cwd(), path.dirname(url.fileURLToPath(import.meta.url)));
+    const fixtures = fs.readFile(path.join(basepath, "./user.json"), "utf8").then((f) => JSON.parse(f));
+    
     it("should include static class 'User'", () => 
         assert.ok(!!SCIMMY.Schemas.User, "Static class 'User' not defined"));
     
     describe("SCIMMY.Schemas.User", () => {
+        describe(".definition", SchemasHooks.definition(SCIMMY.Schemas.User, fixtures));
     });
 }
