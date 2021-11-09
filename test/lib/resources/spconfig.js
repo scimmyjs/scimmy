67eff418350a07068b724cef6d05d7dd9c3b6a79
Sam Lee-Lindsay <sleelin@gmail.com>
2021-11-09 23:29:19 +1100

    Tests(SCIMMY.Resources.ServiceProviderConfig): add initial suite of unit tests

--- test/lib/resources/spconfig.js
+++ test/lib/resources/spconfig.js
@@ -1,9 +1,29 @@
+import {promises as fs} from "fs";
+import path from "path";
+import url from "url";
 import assert from "assert";
 
-export let ServiceProviderConfigSuite = async (SCIMMY) => {
+export let ServiceProviderConfigSuite = (SCIMMY, ResourcesHooks) => {
+    const basepath = path.relative(process.cwd(), path.dirname(url.fileURLToPath(import.meta.url)));
+    const fixtures = fs.readFile(path.join(basepath, "./spconfig.json"), "utf8").then((f) => JSON.parse(f));
+    
     it("should include static class 'ServiceProviderConfig'", () => 
         assert.ok(!!SCIMMY.Resources.ServiceProviderConfig, "Static class 'ServiceProviderConfig' not defined"));
     
     describe("SCIMMY.Resources.ServiceProviderConfig", () => {
+        it("should implement static member 'endpoint' that is a string", ResourcesHooks.endpoint(SCIMMY.Resources.ServiceProviderConfig));
+        it("should not implement static member 'schema'", ResourcesHooks.schema(SCIMMY.Resources.ServiceProviderConfig, false));
+        it("should not implement static member 'extensions'", ResourcesHooks.extensions(SCIMMY.Resources.ServiceProviderConfig, false));
+        it("should override static method 'extend'", ResourcesHooks.extend(SCIMMY.Resources.ServiceProviderConfig, true));
+        it("should not implement static method 'ingress'", ResourcesHooks.ingress(SCIMMY.Resources.ServiceProviderConfig, false));
+        it("should not implement static method 'egress'", ResourcesHooks.egress(SCIMMY.Resources.ServiceProviderConfig, false));
+        it("should not implement static method 'degress'", ResourcesHooks.degress(SCIMMY.Resources.ServiceProviderConfig, false));
+        it("should not implement instance method 'write'", ResourcesHooks.write(SCIMMY.Resources.ServiceProviderConfig, false));
+        it("should not implement instance method 'patch'", ResourcesHooks.patch(SCIMMY.Resources.ServiceProviderConfig, false));
+        it("should not implement instance method 'dispose'", ResourcesHooks.dispose(SCIMMY.Resources.ServiceProviderConfig, false));
+        
+        describe(".basepath()", ResourcesHooks.basepath(SCIMMY.Resources.ServiceProviderConfig));
+        describe("#constructor", ResourcesHooks.construct(SCIMMY.Resources.ServiceProviderConfig, false));
+        describe("#read()", ResourcesHooks.read(SCIMMY.Resources.ServiceProviderConfig, fixtures, false));
     });
 }
