860d1455b8669b07ea8000bbcf06c2526f540797
Sam Lee-Lindsay <sleelin@gmail.com>
2021-11-22 14:27:46 +1100

    Add(SCIMMY.Messages.BulkOp): handling of bulkId including circular references

--- src/lib/messages/bulkop.js
+++ src/lib/messages/bulkop.js
@@ -57,6 +57,13 @@ export class BulkOp {
     #bulkOperations;
     
     /**
+     * POST operations that will have their bulkId resolved into a real ID
+     * @type {Map<String, Object>}
+     * @private
+     */
+    #bulkIds;
+    
+    /**
      * Instantiate a new SCIM BulkResponse message from the supplied BulkRequest
      * @param {Object} request - contents of the BulkRequest operation being performed
      * @param {Object[]} request.Operations - list of SCIM-compliant bulk operations to apply
@@ -89,9 +96,19 @@ export class BulkOp {
         this.schemas = [BulkOp.#responseId];
         this.Operations = [];
         
+        // Get a list of POST ops with bulkIds for direct and circular reference resolution
+        let postOps = operations.filter(o => o.method === "POST" && !!o.bulkId && typeof o.bulkId === "string");
+        
         // Store details of BulkRequest to be applied
         this.#errorLimit = failOnErrors;
         this.#bulkOperations = operations;
+        this.#bulkIds = new Map(postOps.map(({bulkId}) => {
+            // Establish who waits on what, and provide a way for that to happen
+            let handlers = {referencedBy: postOps.filter(({data}) => JSON.stringify(data).includes(`bulkId:${bulkId}`)).map(({bulkId}) => bulkId)},
+                value = new Promise((resolve, reject) => Object.assign(handlers, {resolve: resolve, reject: reject}));
+            
+            return [bulkId, Object.assign(value, handlers)];
+        }));
     }
     
     /**
@@ -104,89 +121,161 @@ export class BulkOp {
         if (!resourceTypes.every(r => r.prototype instanceof Types.Resource))
             throw new TypeError("Expected 'resourceTypes' parameter to be an array of Resource type classes in 'apply' method of BulkOp");
         
-        // Set up easy access to resource types by endpoint, and bulkId to real ID map
+        // Set up easy access to resource types by endpoint, and store pending results
         let typeMap = new Map(resourceTypes.map((r) => [r.endpoint, r])),
-            bulkIds = new Map();
+            bulkIdTransients = [...this.#bulkIds.keys()],
+            lastErrorIndex = this.#bulkOperations.length + 1,
+            results = [];
         
-        for (let op of this.#bulkOperations) {
-            if (!this.#errorLimit || this.#errorCount < this.#errorLimit) {
-                let {method, bulkId, path = "", data} = op,
-                    index = this.#bulkOperations.indexOf(op) + 1,
-                    errorSuffix = `in BulkRequest operation #${index}`,
-                    [endpoint, id] = path.substring(1).split("/"),
+        for (let op of this.#bulkOperations) results.push((async () => {
+            // Unwrap useful information from the operation
+            let {method, bulkId, path = "", data} = op,
+                // Evaluate endpoint and resource ID, and thus what kind of resource we're targeting 
+                [endpoint, id] = path.substring(1).split("/"),
                     TargetResource = (endpoint ? typeMap.get(`/${endpoint}`) : false),
-                    location = (TargetResource ? [TargetResource.basepath() ?? TargetResource.endpoint, id].filter(v => v).join("/") : path),
-                    result = {method: method, bulkId: bulkId, location: location},
-                    error = false;
+                // Construct a location for the response, and prepare common aspects of the result
+                location = (TargetResource ? [TargetResource.basepath() ?? TargetResource.endpoint, id].filter(v => v).join("/") : path),
+                result = {method: method, bulkId: bulkId, location: location},
+                // Find out if this op waits on any other operations 
+                jsonData = (!!data ? JSON.stringify(data) : ""),
+                waitingOn = (!jsonData.includes("bulkId:") ? [] : [...new Set([...jsonData.matchAll(/"bulkId:(.+?)"/g)].map(([, id]) => id))]),
+                // Establish error handling
+                index = this.#bulkOperations.indexOf(op) + 1,
+                errorSuffix = `in BulkRequest operation #${index}`,
+                error = false;
+            
+            // Ignore the bulkId unless method is POST
+            bulkId = (String(method).toUpperCase() === "POST" ? bulkId : false);
+            
+            // If not the first operation, and there's no circular references, wait on all prior operations
+            if (index > 1 && (!bulkId || !waitingOn.length || !waitingOn.some(id => this.#bulkIds.get(bulkId).referencedBy.includes(id)))) {
+                let lastOp = (await Promise.all(results.slice(0, index - 1))).pop();
                 
-                // Preemptively add the result to the stack
-                this.Operations.push(result);
-                
-                // Make sure method has a value
-                if (!method && method !== false)
-                    error = new ErrorMessage(new Types.Error(400, "invalidSyntax", `Missing or empty 'method' string ${errorSuffix}`));
-                // Make sure that value is a string
-                else if (typeof method !== "string")
-                    error = new ErrorMessage(new Types.Error(400, "invalidSyntax", `Expected 'method' to be a string ${errorSuffix}`));
-                // Make sure that string is a valid method
-                else if (!validMethods.includes(String(method).toUpperCase()))
-                    error = new ErrorMessage(new Types.Error(400, "invalidValue", `Invalid 'method' value '${method}' ${errorSuffix}`));
-                // Make sure path has a value
-                else if (!path && path !== false)
-                    error = new ErrorMessage(new Types.Error(400, "invalidSyntax", `Missing or empty 'path' string ${errorSuffix}`));
-                // Make sure that path is a string
-                else if (typeof path !== "string")
-                    error = new ErrorMessage(new Types.Error(400, "invalidSyntax", `Expected 'path' to be a string ${errorSuffix}`));
-                // Make sure that string points to a valid resource type
-                else if (![...typeMap.keys()].some(e => path.startsWith(e)))
-                    error = new ErrorMessage(new Types.Error(400, "invalidValue", `Invalid 'path' value '${path}' ${errorSuffix}`));
-                // Make sure there ISN'T a resource targeted if the request type is POST
-                else if (method.toUpperCase() === "POST" && !!id)
-                    error = new ErrorMessage(new Types.Error(404, null, "POST operation must not target a specific resource"));
-                // Make sure there IS a resource targeted if the request type isn't POST
-                else if (method.toUpperCase() !== "POST" && !id)
-                    error = new ErrorMessage(new Types.Error(404, null, `${method.toUpperCase()} operation must target a specific resource`));
-                // Make sure data is an object, if method isn't DELETE
-                else if (method.toUpperCase() !== "DELETE" && (Object(data) !== data || Array.isArray(data)))
-                    error = new ErrorMessage(new Types.Error(400, "invalidSyntax", `Expected 'data' to be a single complex value ${errorSuffix}`))
-                // If things look OK, attempt to apply the operation
-                else {
-                    try {
-                        let resource = new TargetResource(id),
-                            value;
+                // If the last operation failed, and error limit reached, bail out here
+                if (!lastOp || (lastOp.response instanceof ErrorMessage && !(!this.#errorLimit || (this.#errorCount < this.#errorLimit))))
+                    return;
+            }
+            
+            // Make sure method has a value
+            if (!method && method !== false)
+                error = new ErrorMessage(new Types.Error(400, "invalidSyntax", `Missing or empty 'method' string ${errorSuffix}`));
+            // Make sure that value is a string
+            else if (typeof method !== "string")
+                error = new ErrorMessage(new Types.Error(400, "invalidSyntax", `Expected 'method' to be a string ${errorSuffix}`));
+            // Make sure that string is a valid method
+            else if (!validMethods.includes(String(method).toUpperCase()))
+                error = new ErrorMessage(new Types.Error(400, "invalidValue", `Invalid 'method' value '${method}' ${errorSuffix}`));
+            // Make sure path has a value
+            else if (!path && path !== false)
+                error = new ErrorMessage(new Types.Error(400, "invalidSyntax", `Missing or empty 'path' string ${errorSuffix}`));
+            // Make sure that path is a string
+            else if (typeof path !== "string")
+                error = new ErrorMessage(new Types.Error(400, "invalidSyntax", `Expected 'path' to be a string ${errorSuffix}`));
+            // Make sure that string points to a valid resource type
+            else if (![...typeMap.keys()].includes(`/${endpoint}`))
+                error = new ErrorMessage(new Types.Error(400, "invalidValue", `Invalid 'path' value '${path}' ${errorSuffix}`));
+            // Make sure there IS a bulkId if the method is POST
+            else if (method.toUpperCase() === "POST" && !bulkId && bulkId !== false)
+                error = new ErrorMessage(new Types.Error(400, "invalidSyntax", `POST operation missing required 'bulkId' string ${errorSuffix}`));
+            // Make sure there IS a bulkId if the method is POST
+            else if (method.toUpperCase() === "POST" && typeof bulkId !== "string")
+                error = new ErrorMessage(new Types.Error(400, "invalidValue", `POST operation expected 'bulkId' to be a string ${errorSuffix}`));
+            // Make sure there ISN'T a resource targeted if the method is POST
+            else if (method.toUpperCase() === "POST" && !!id)
+                error = new ErrorMessage(new Types.Error(404, null, `POST operation must not target a specific resource ${errorSuffix}`));
+            // Make sure there IS a resource targeted if the method isn't POST
+            else if (method.toUpperCase() !== "POST" && !id)
+                error = new ErrorMessage(new Types.Error(404, null, `${method.toUpperCase()} operation must target a specific resource ${errorSuffix}`));
+            // Make sure data is an object, if method isn't DELETE
+            else if (method.toUpperCase() !== "DELETE" && (Object(data) !== data || Array.isArray(data)))
+                error = new ErrorMessage(new Types.Error(400, "invalidSyntax", `Expected 'data' to be a single complex value ${errorSuffix}`))
+            // Make sure any bulkIds referenced in data can eventually be resolved
+            else if (!waitingOn.every((id) => this.#bulkIds.has(id)))
+                error = new ErrorMessage(new Types.Error(400, "invalidValue", `No POST operation found matching bulkId '${waitingOn.find((id) => !this.#bulkIds.has(id))}'`));
+            // If things look OK, attempt to apply the operation
+            else {
+                try {
+                    // Go through and wait on any referenced POST bulkIds
+                    for (let referenceId of waitingOn) {
+                        // Find the referenced operation to wait for
+                        let reference = this.#bulkIds.get(referenceId),
+                            referenceIndex = bulkIdTransients.indexOf(referenceId);
                         
-                        switch (method.toUpperCase()) {
-                            case "POST":
-                            case "PUT":
-                                value = await resource.write(data);
-                                if (bulkId && value.id) bulkIds.set(bulkId, value.id);
-                                Object.assign(result, {status: (!!id ? "200" : "201"), location: value?.meta?.location});
-                                break;
-                                
-                            case "PATCH":
-                                value = await resource.patch(data);
-                                Object.assign(result, {status: (value ? "200" : "204")}, (value ? {location: value?.meta?.location} : {}));
-                                break;
-                                
-                            case "DELETE":
-                                await resource.dispose();
-                                Object.assign(result, {status: "204"});
-                                break;
+                        // If the reference is also waiting on us, we have ourselves a circular reference!
+                        if (bulkId && !id && reference.referencedBy.includes(bulkId) && (bulkIdTransients.indexOf(bulkId) < referenceIndex)) {
+                            // Attempt to POST self without reference so referenced operation can complete and give us its ID!
+                            ({id} = await new TargetResource().write(Object.entries(data)
+                                // Remove any values that reference a bulkId
+                                .filter(([,v]) => !JSON.stringify(v).includes("bulkId:"))
+                                .reduce((res, [k, v]) => (((res[k] = v) || true) && res), {})));
+                            
+                            // Set the ID for future use and resolve pending references
+                            jsonData = JSON.stringify(Object.assign(data, {id: id}));
+                            this.#bulkIds.get(bulkId).resolve(id);
                         }
-                    } catch (ex) {
-                        // Set the error variable for final handling
-                        error = new ErrorMessage(ex);
+                        
+                        try {
+                            // Replace reference with real value once resolved
+                            jsonData = jsonData.replaceAll(`bulkId:${referenceId}`, await reference);
+                            data = JSON.parse(jsonData);
+                        } catch (ex) {
+                            // Referenced POST operation precondition failed, remove any created resource and bail out
+                            if (bulkId && id) await new TargetResource(id).dispose();
+                            
+                            // If we're following on from a prior failure, no need to explain why, otherwise, explain the failure
+                            if (ex instanceof ErrorMessage && (!!this.#errorLimit && this.#errorCount >= this.#errorLimit && index > lastErrorIndex)) return;
+                            else throw new Types.Error(412, null, `Referenced POST operation with bulkId '${referenceId}' was not successful`);
+                        }
                     }
-                }
-                
-                // If there was an error, store result and increment error count
-                if (error instanceof ErrorMessage) {
-                    Object.assign(result, {status: error.status, response: error, location: (String(method).toUpperCase() !== "POST" ? result.location : undefined)});
-                    this.#errorCount++;
+                    
+                    // Get ready
+                    let resource = new TargetResource(id),
+                        value;
+                    
+                    // Do the thing!
+                    switch (method.toUpperCase()) {
+                        case "POST":
+                        case "PUT":
+                            value = await resource.write(data);
+                            if (bulkId && !resource.id && value.id) this.#bulkIds.get(bulkId).resolve(value.id); 
+                            Object.assign(result, {status: (!bulkId ? "200" : "201"), location: value?.meta?.location});
+                            break;
+                            
+                        case "PATCH":
+                            value = await resource.patch(data);
+                            Object.assign(result, {status: (value ? "200" : "204")}, (value ? {location: value?.meta?.location} : {}));
+                            break;
+                            
+                        case "DELETE":
+                            await resource.dispose();
+                            Object.assign(result, {status: "204"});
+                            break;
+                    }
+                } catch (ex) {
+                    // Coerce the exception into a SCIMError
+                    if (!(ex instanceof Types.Error)) 
+                        ex = new Types.Error(...(ex instanceof TypeError ? [400, "invalidValue"] : [500, null]), ex.message);
+                    
+                    // Set the error variable for final handling, and reject any pending operations
+                    error = new ErrorMessage(ex);
                 }
             }
-        }
+            
+            // If there was an error, store result and increment error count
+            if (error instanceof ErrorMessage) {
+                Object.assign(result, {status: error.status, response: error, location: (String(method).toUpperCase() !== "POST" ? result.location : undefined)});
+                lastErrorIndex = (index < lastErrorIndex ? index : lastErrorIndex);
+                this.#errorCount++;
+                
+                // Also reject the pending bulkId promise as no resource ID can exist
+                if (bulkId) this.#bulkIds.get(bulkId).reject(error);
+            }
+            
+            return result;
+        })());
         
+        // Store the results and return the BulkOp for chaining
+        this.Operations.push(...(await Promise.all(results)).filter(r => r));
         return this;
     }
 }
