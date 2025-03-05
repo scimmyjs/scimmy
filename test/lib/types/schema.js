e6caa199d301fd5b59805f859a48fb057996c75e
Sam Lee-Lindsay <sleelin@gmail.com>
2025-03-05 14:34:17 +1100

    Test(SCIMMY.Types.Schema): add missing branch coverage

--- test/lib/types/schema.js
+++ test/lib/types/schema.js
@@ -42,12 +42,35 @@ describe("SCIMMY.Types.Schema", () => {
             assert.ok(typeof Schema.extend === "function",
                 "Static method 'extend' was not implemented");
         });
+        
+        it("should expect 'extension' argument to be a Schema class, SchemaDefinition instance, or collection of Attribute instances", () => {
+            assert.throws(() => Schema.extend({}), 
+                {name: "TypeError", message: "Expected 'extension' to be a Schema class, SchemaDefinition instance, or collection of Attribute instances"},
+                "Static method 'extend' did not throw with invalid object input");
+            assert.throws(() => Schema.extend([new Attribute("string", "test"), {}]),
+                {name: "TypeError", message: "Expected 'extension' to be a Schema class, SchemaDefinition instance, or collection of Attribute instances"},
+                "Static method 'extend' did not throw with invalid object input");
+        });
     });
     
     describe(".truncate()", () => {
         it("should be implemented", () => {
             assert.ok(typeof Schema.truncate === "function",
                 "Static method 'truncate' was not implemented");
+        });
+        
+        it("should resolve schema definition instances from Schema classes", () => {
+            const attributes = [new Attribute("string", "aValue"), new Attribute("string", "aString", {returned: false})];
+            const Test = createSchemaClass({attributes});
+            const Extension = createSchemaClass({name: "Extension", id: Test.definition.id.replace("Test", "Extension")});
+            
+            Test.extend(Extension);
+            
+            try {
+                Test.truncate(Extension);
+            } catch (ex) {
+                assert.fail(`Static method 'truncate' did not resolve schema definition instance from Schema class\r\n[cause]: ${ex}`);
+            }
         });
     });
     
@@ -63,6 +86,21 @@ describe("SCIMMY.Types.Schema", () => {
             
             assert.deepStrictEqual(JSON.parse(JSON.stringify(actual)), expected,
                 "Schema instance did not include 'toJSON' method that strips attributes where returned is marked as 'never'");
+        });
+        
+        it("should include 'toJSON' method on schema extension values", () => {
+            const Test = createSchemaClass();
+            const attributes = [new Attribute("string", "aValue"), new Attribute("string", "aString", {returned: false})];
+            const Extension = createSchemaClass({name: "Extension", id: Test.definition.id.replace("Test", "Extension"), attributes});
+            const source = {aValue: "a value"};
+            const expected = {schemas: [Test.definition.id, Extension.definition.id], meta: {resourceType: Test.definition.name}, [Extension.definition.id]: source};
+            
+            Test.extend(Extension);
+            
+            const actual = new Test({[Extension.definition.id]: {...source, aString: "a string"}});
+            
+            assert.deepStrictEqual(JSON.parse(JSON.stringify(actual)), expected,
+                "Schema instance did not include 'toJSON' method on schema extension values");
         });
     });
 });
