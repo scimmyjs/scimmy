682e8b234f9333d226cd4dd9f799a949adf8d392
Matt Basta <matt@runwayml.com>
2025-08-28 13:54:53 -0400

    Use correct enum types

--- src/lib/types/error.js
+++ src/lib/types/error.js
@@ -10,11 +10,11 @@
 export class SCIMError extends Error {
     /**
      * Instantiate a new error with SCIM error details
-     * @param {Number} status - HTTP status code to be sent with the error
-     * @param {String|null} scimType - the SCIM detail error keyword as per [RFC7644§3.12]{@link https://datatracker.ietf.org/doc/html/rfc7644#section-3.12}
+     * @param {SCIMMY.Messages.ErrorResponse~ValidStatusCodes} status - HTTP status code to be sent with the error
+     * @param {SCIMMY.Messages.ErrorResponse~ValidScimTypes|null} scimType - the SCIM detail error keyword as per [RFC7644§3.12]{@link https://datatracker.ietf.org/doc/html/rfc7644#section-3.12}
      * @param {String} message - a human-readable description of what caused the error to occur
-     * @property {Number} status - HTTP status code to be sent with the error
-     * @property {String|null} scimType - the SCIM detail error keyword as per [RFC7644§3.12]{@link https://datatracker.ietf.org/doc/html/rfc7644#section-3.12}
+     * @property {SCIMMY.Messages.ErrorResponse~ValidStatusCodes} status - HTTP status code to be sent with the error
+     * @property {SCIMMY.Messages.ErrorResponse~ValidScimTypes|null} scimType - the SCIM detail error keyword as per [RFC7644§3.12]{@link https://datatracker.ietf.org/doc/html/rfc7644#section-3.12}
      * @property {String} message - a human-readable description of what caused the error to occur
      */
     constructor(status, scimType, message) {
